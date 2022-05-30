62aced29c22443ee2281b8723ead8891efbb0355
Mattia Procopio <promat85@gmail.com>
2022-05-30 09:41:46 +0200

    Prepare to add non serial devices
    Also started documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,71 @@
-
+//! The basic building blocks you need to build your next astrophotography suite.
+//!
+//! Astrotools provides traits and utils that can be used to implement
+//! multiplatform drivers to drive astronomical equipment.
 use lightspeed_astro::devices::actions::DeviceActions;
 use lightspeed_astro::props::Property;
 use uuid::Uuid;
 
-pub trait AstronomicalDevice {
+
+pub trait AstroSerialDevice {
+    /// Main and only entrypoint to create a new serial device.
+    ///
+    /// A device that doesn't work/cannot communicate with is not really useful
+    /// so this may return `None` if there is something wrong with the just
+    /// discovered device.
     fn new(name: &str, address: &str, baud: u32, timeout_ms: u64) -> Option<Self>
     where
         Self: Sized;
+
+    /// Implement this method with the actual logic to issue serial commands
+    /// to a  device.
     fn send_command<C: std::fmt::UpperHex>(
         &mut self,
         comm: C,
         val: Option<String>,
     ) -> Result<String, DeviceActions>;
+
+    /// Use this method to fetch the real properties from the device,
+    /// this should not be called directly from clients ideally,
+    /// for that goal `get_properties` should be used.
     fn fetch_props(&mut self);
+
+    /// Use this method to return the id of the device as a uuid.
     fn get_id(&self) -> Uuid;
+
+    /// Return the OS address of the device (/dev/ttyUSB0 or /COM6 for example).
     fn get_address(&self) -> &String;
+
+    /// Use this method to return the name of the device (e.g. ZWO533MC).
     fn get_name(&self) -> &String;
+
+    /// Use this method to return the actual cached state stored into `self.properties`.
     fn get_properties(&self) -> &Vec<Property>;
+
+    /// Method to be used when receving requests from clients to update properties.
+    ///
+    /// Ideally this should call internally `update_property_remote` which will be
+    /// responsible to trigger the action against the device to update the property
+    /// on the device itself, if the action is successful the last thing this method
+    /// does would be to update the property inside `self.properties`.
     fn update_property(&mut self, prop_name: &str, val: &str) -> Result<(), DeviceActions>;
+
+    /// Use this method to send a command to the device to change the requested property.
+    ///
+    /// Ideally this method will be a big `match` clause where the matching will execute
+    /// `self.send_command` to issue a serial command to the device.
     fn update_property_remote(&mut self, prop_name: &str, val: &str) -> Result<(), DeviceActions>;
+
+    /// Properties are packed into a vector so to find them we need to
+    /// lookup the index, use this method to do so.
     fn find_property_index(&self, prop_name: &str) -> Option<usize>;
 }
 
+/// Module that gathers all functions that may help debugging or working woth devices.
 pub mod utils {
-    use crate::AstronomicalDevice;
+    use crate::AstroSerialDevice;
 
-    pub fn print_device_table<T: AstronomicalDevice>(devices: &Vec<T>) {
+    pub fn print_device_table<T: AstroSerialDevice>(devices: &Vec<T>) {
         for d in devices {
             println!("");
             println!("=======================================");
